fbf7f2bb78046a5a44bca50157b132ef31fdac00
William <liams_1998@hotmail.com>
2019-07-24 23:51:12 +0300

    created contact us form

--- washy-wash/src/App.js
+++ washy-wash/src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
   render() { 
     return (  
       <div>
-        {/* <ContactUs /> */}
+        <ContactUs />
         {/* <MyOrders /> */}
         {/* <ViewOrders /> */}
          {/* <LandingPage />  */}
--- washy-wash/src/components/ContactUsForm/ContactUsForm.js
+++ washy-wash/src/components/ContactUsForm/ContactUsForm.js
@@ -1,5 +1,8 @@
 import React from "react";
-import "./ContactUsForm.css";
+
+import { Form, Col, Button } from "react-bootstrap";
+
+import "./ContactUsForm.scss";
 
 class ContactUsForm extends React.Component {
   constructor(props) {
@@ -9,7 +12,42 @@ class ContactUsForm extends React.Component {
   render() {
     return (
       <div className="ContactUsForm-Container">
-        
+        <Form md="5">
+          <h2>Contact Us</h2>
+          <Form.Row>
+            <Col md="6">
+              <Form.Label>Name*</Form.Label>
+              <Form.Control type="text" required />
+            </Col>
+            <Col md="6">
+              <Form.Label>LastName*</Form.Label>
+              <Form.Control type="text" required />
+            </Col>
+          </Form.Row>
+          <Form.Row>
+            <Form.Label>Email*</Form.Label>
+            <Form.Control type="email" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Label>Phone*</Form.Label>
+            <Form.Control type="tel" required />
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Label>Message</Form.Label>
+            <Form  as="textarea" rows="3" />
+          </Form.Row>
+
+          <Form.Row>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form.Row>
+        </Form>
       </div>
     );
   }
