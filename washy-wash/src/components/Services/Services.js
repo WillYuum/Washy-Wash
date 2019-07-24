ad1fb14484468b5122575cd9ab94b74e680bb827
William <liams_1998@hotmail.com>
2019-07-24 11:15:07 +0300

    testing Service.js

--- washy-wash/src/components/Services/Services.js
+++ washy-wash/src/components/Services/Services.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./Services.css"
 
+import ServiceBox from "./ServicesBox/ServiceBox.js"
 
 
 const Services = () => {
     return ( 
-        <div>
-
+        <div className = "Services-container">
+            <ServiceBox clothImg = "https://www.rd.com/wp-content/uploads/2018/02/01_Lights_Laundry-Mistakes-You-Didn%E2%80%99t-Know-You-Were-Making_478483900_Evgeny-Atamanenko-760x506.jpg"
+                        clothType = "Pants"
+                        clothPrice = "$12" />
         </div>
      );
 }
