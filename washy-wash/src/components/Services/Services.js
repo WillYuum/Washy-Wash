c97e55f8f5d2b27f933f78cfdc1a5c00441a39f4
HovigKevorkian <hovikkevork@gmail.com>
2019-07-24 11:17:09 +0300

    wow

--- washy-wash/src/components/Services/Services.js
+++ washy-wash/src/components/Services/Services.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import "./Services.css"
-
+servicesList=
+[
+    {ID:1 , Photo:"https://www.ghanayello.com/img/gh/h/1500992537-57-k-a-cleaning-and-laundry-services.jpg", Title:first, Price:5},
+    {ID:1 , Photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFfmbIoSaf-epiY4wrRfU08gehkIgXMSnRH15DY_7TFBwWT4BA", Title:first, Price:5},
+    {ID:1 , Photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFfmbIoSaf-epiY4wrRfU08gehkIgXMSnRH15DY_7TFBwWT4BA", Title:first, Price:5},
+    {ID:1 , Photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFfmbIoSaf-epiY4wrRfU08gehkIgXMSnRH15DY_7TFBwWT4BA", Title:first, Price:5},
+    {ID:1 , Photo:"https://s3-media3.fl.yelpcdn.com/bphoto/Uf-keIaU8qnNx8mjhZL5HQ/ls.jpg", Title:first, Price:5},
+    {ID:1 , Photo:"https://webimg.secondhandapp.com/w-i-mgl/5c5d82308c84bb0b5c11ebcd", Title:first, Price:5},
+    {ID:1 , Photo:"http://www.benchmarkcleaning.ca/resources/Cleaning-breakdown.png", Title:first, Price:5},
+   
+]
 
 
 const Services = () => {
