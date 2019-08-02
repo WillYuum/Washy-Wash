65d8a7b16d6f4fa4f6e974914ceeb212c4bf661d
William <liams_1998@hotmail.com>
2019-08-02 11:37:55 +0300

    fetch

--- washy-wash/src/Pages/MainPage/Main.js
+++ washy-wash/src/Pages/MainPage/Main.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import Hero from "../../components/Hero/Hero.js"
-import HowWeWork from "../../components/HowWeWork/HowWeWork.js"
-import Services from "../../components/Services/Services.js"
-import CookieConsent from "react-cookie-consent";
+import React from "react";
+import Hero from "../../components/Hero/Hero.js";
+import HowWeWork from "../../components/HowWeWork/HowWeWork.js";
+import Services from "../../components/Services/Services.js";
 import CookieConsent, { Cookies } from "react-cookie-consent";
 class Main extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        return (
-            <div>
-                       
-<Hero />
-<HowWeWork />
-<Services />
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <Hero />
+        <HowWeWork />
+        <Services />
+      </div>
+    );
+  }
 }
 
 export default Main;
-
